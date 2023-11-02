9961d3a26f5c685e54f6ef9780f6ea09553671bc
canisminor1990 <i@canisminor.cc>
2023-11-03 00:08:09 +0800

    ✨ feat: Init

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import qs from 'query-string';
+
+import cors from '../src/cors';
+import { SsmlOptions } from '../src/genSSML';
+import { postMicrosoftSpeech } from '../src/postMicrosoftSpeech';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default async (req: Request) => {
+  const { text, ...options }: SsmlOptions & { text: string } = qs.parseUrl(req.url).query as any;
+
+  const res = await fetch(...postMicrosoftSpeech(text, options));
+  return cors(req, res);
+};
