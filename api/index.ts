68a81b3371f58ee00107cc4a0d8cd8f2bd4422a9
CanisMinor <i@canisminor.cc>
2023-11-03 16:02:44 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import qs from 'query-string';
 
-// import cors from '../src/cors';
+import cors from '../src/cors';
 import { SsmlOptions } from '../src/genSSML';
 import { postMicrosoftSpeech } from '../src/index';
 
@@ -12,5 +12,10 @@ export default async (req: Request) => {
   const { text, ...options }: SsmlOptions & { text: string } = qs.parseUrl(req.url).query as any;
 
   const res = await fetch(...postMicrosoftSpeech(text, options));
-  return res
+
+  const headers = new Headers(res.headers);
+
+  res.headers = headers;
+  
+  return cors(req, res);
 };
