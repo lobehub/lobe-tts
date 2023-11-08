159916cd3aeb6c3c897c876060d4b7f03d5c0e5a
CanisMinor <i@canisminor.cc>
2023-11-09 01:58:25 +0800

    🐛 fix: Fix vercel deploy error

--- api/azure-speech.ts
+++ api/azure-speech.ts
@@ -1,3 +1,9 @@
+// TODO: fix vercel error
+// Error: The Edge Function "api/azure-speech" is referencing unsupported modules:
+// - https-proxy-agent: net, tls, url
+// - microsoft-cognitiveservices-speech-sdk: vc-blob-asset:speech-processor.js, fs, net, tls
+
+/*
 import cors from '../src/server/cors';
 import { getAllowOrigins } from '../src/server/getAllowOrigins';
 import { handleAzureSpeechRequest } from '../src/server/handleAzureSpeechRequest';
@@ -12,4 +18,11 @@ export default async (req: Request) => {
   if (!origin) return new Response('Origin Not Allowed', { status: 403 });
   const res = await handleAzureSpeechRequest(req);
   return cors(req, res, { methods: ['POST'], origin });
+};
+*/
+
+export default async (req: Request) => {
+  if (req.method !== 'POST') return new Response('Method Not Allowed', { status: 405 });
+
+  return new Response('WIP');
 };
