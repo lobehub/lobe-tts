a4a41e645cd12feb711d057749fef708aa095a9f
hedeqiang <laravel_code@163.com>
2025-03-29 01:25:44 +0800

    feat: update voicelist

--- src/core/OpenAITTS/voiceList.ts
+++ src/core/OpenAITTS/voiceList.ts
@@ -1,6 +1,6 @@
 import type { SelectProps } from 'antd';
 
-const voiceList = ['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer'] as const;
+const voiceList = ['alloy', 'ash', 'ballad', 'coral', 'echo', 'fable', 'onyx', 'nova', 'sage', 'shimmer'] as const;
 export default voiceList;
 
 export const getOpenaiVoiceOptions = (): SelectProps['options'] => {
